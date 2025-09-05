c86bf52b81e0bf7c02dcf5cd37c5f66dd5b21632
Martin Lumumba <55294377+Martinlmb3@users.noreply.github.com>
2025-09-05 16:35:35 +0200

    fix class issue

--- app/Models/pokedex.php
+++ app/Models/pokedex.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class pokedex extends Model
+class Pokedex extends Model
 {
     /** @use HasFactory<\Database\Factories\PokedexFactory> */
     use HasFactory;
